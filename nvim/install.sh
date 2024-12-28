96e0d31bdc31a8e1faaa4aa9cba1493550bf0c04
Shoresy <shoresylx@proton.me>
2024-12-27 16:47:13 -0800

    Fix nvim install

--- nvim/install.sh
+++ nvim/install.sh
@@ -2,12 +2,12 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-sudo pacman -S --needed --noconfirm neovim python-rope python-pyflakes python-macabe python-pycodestyle yapf python-whatthepatch python-lsp-server
+sudo pacman -S --needed --noconfirm neovim python-rope python-pyflakes python-mccabe python-pycodestyle yapf python-whatthepatch python-lsp-server
 
-if [ ! -d "$HOME/.config/nvim"]; then
+if [ ! -d "$HOME/.config/nvim" ]; then
 	mkdir $HOME/.config/nvim
 fi
 
 for filename in $SCRIPT_DIR/*; do
-	ln -sfn $SCRIPT_DIR/$filename $HOME/.config/nvim/$filename
+	ln -sfn $filename $HOME/.config/nvim/
 done
