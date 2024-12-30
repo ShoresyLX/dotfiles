de1025c78aef8259d25324846afbfb456111c245
Shoresy <shoresylx@proton.me>
2024-12-29 16:45:52 -0800

    Started on NvimTree keybinds

--- nvim/init.lua
+++ nvim/init.lua
@@ -114,6 +114,10 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Set bind to open and close nvimtree
+vim.keymap.set('n', '<leader>po', '<cmd>NvimTreeOpen<CR>', { desc = '[O]pen NvimTree' })
+vim.keymap.set('n', '<leader>pc', '<cmd>NvimTreeClose<CR>', { desc = '[C]lose NvimTree' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
